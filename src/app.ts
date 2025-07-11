f247936e79d50efb7074dc36619c934b12250e14
habibur5313 <habiburmamun313@gmail.com>
2025-07-11 09:28:28 +0600

    part-1 completed

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,16 @@
 import cors from "cors";
 import express, { Request, Response } from "express";
-// import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
-// import notFound from "./app/middlewares/notFound";
-// import { router } from "./app/routes";
+import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
+import { router } from "./app/routes";
+import notFound from "./app/middlewares/notFound";
+
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 
-// app.use("/api/v1", router)
+app.use("/api/v1", router)
 
 app.get("/", (req: Request, res: Response) => {
     res.status(200).json({
@@ -18,8 +19,8 @@ app.get("/", (req: Request, res: Response) => {
 })
 
 
-// app.use(globalErrorHandler)
+app.use(globalErrorHandler)
 
-// app.use(notFound)
+app.use(notFound)
 
 export default app
